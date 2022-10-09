14c7e9181a8c5fbd91022bb0ad237ea6b664cbd6
rohityadav4422 <115135362+rohityadav4422@users.noreply.github.com>
2022-10-09 14:50:45 +0530

    Add files via upload

---
+++ StudentController.java
@@ -0,0 +1,57 @@
+package com.example.demo.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.DTO.StudentDTO;
+import com.example.demo.Response.StudentResponse;
+import com.example.demo.Service.StudentService;
+
+
+@RestController
+public class StudentController {
+
+	@Autowired
+	private StudentService service;
+
+	@PostMapping("/insert")
+	private ResponseEntity<StudentResponse> insertStudent(@RequestBody StudentDTO student) {
+		StudentDTO insertedStudent = service.insertStudent(student);
+		return new ResponseEntity<StudentResponse>(new StudentResponse("OK", "Student inserted", insertedStudent, null),
+				HttpStatus.ACCEPTED);
+	}
+
+	@GetMapping("/searchByName")
+	private ResponseEntity<StudentResponse> searchByName(@RequestBody StudentDTO student) {
+		StudentDTO searchByName = service.searchByName(student.getName());
+		return new ResponseEntity<StudentResponse>(new StudentResponse("OK", "Student inserted", searchByName, null),
+				HttpStatus.ACCEPTED);
+	}
+    
+	@GetMapping("/findAll")
+	private ResponseEntity<StudentResponse> findAll(){
+		List<StudentDTO> student=service.findAll();
+		return new ResponseEntity<StudentResponse>(new StudentResponse("OK", "Student inserted", null, student),
+				HttpStatus.ACCEPTED);
+		
+	}
+	
+	@PostMapping("/delete")
+	private ResponseEntity<StudentResponse> delete(int id){
+		StudentDTO delete=service.delete(id);
+		
+		return new ResponseEntity<StudentResponse>(new StudentResponse("OK", "Student Deleted", delete, null),HttpStatus.ACCEPTED);
+		
+		
+	
+		
+	}
+	
+}
